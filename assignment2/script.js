92cea05f4bd4fb287f3fa725349caa1f145f7dd3
spikkernamaumbo <bsc-53-16@cc.ac.mw>
2019-03-25 01:53:42 -1200

    'assignment2'

---
+++ assignment2/script.js
@@ -0,0 +1,23 @@
+var form =  document.getElementById("form");
+
+form.addEventListener("submit", function(event) {
+	event.preventDefault();
+	var name = form.name.value;
+	var decrpt = form.Project_Description.value;
+	var date = new Date(form.due_date.value);
+
+	let arr = [name, decrpt, date];
+
+	localStorage.setItem("1", arr);
+
+	var display = document.getElementById("display");
+	var links = document.createElement("A");
+	links.setAttribute("href", "project.html");
+	links.setAttribute("class", "link");
+	var project = document.createTextNode(name);
+	links.appendChild(project);
+	display.appendChild(links);
+	form.reset();
+
+
+});
